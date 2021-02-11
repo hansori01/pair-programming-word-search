b0677c0e070ebef63175a797fbf27eb13ad2f6dd
hansori01 <19sorihan88@gmail.com>
2021-02-11 01:37:41 +0000

    solved stretch (not diag.) and eslint

--- wordsearch.js
+++ wordsearch.js
@@ -1,50 +1,42 @@
 //Pair programming with Kyle Lemmon
 //We were unable to finish stretch due to time. May come back
 
-const wordSearch = (letters, word) => { 
-    //joins letters into horizontal arrays
-    let horizontalJoin = letters.map(ls => ls.join(''))
-    //loop through horizontal arrays
-    for (let l of horizontalJoin) {
-        //if current array includes word, return true;
-        if (l.includes(word)) return true;
+const wordSearch = (letters, word) => {
+  //joins letters into horizontal arrays
+  let horizontalJoin = letters.map(ls => ls.join(''));
+  //loop through horizontal arrays
+  for (let l of horizontalJoin) {
+    //if current array includes word, return true;
+    if (l.includes(word)) return true;
+  }
+
+  //if case above is false, try flipping
+  const verticalToHorizontal = [];
+  for (let j = 0; j < letters[0].length; j++) {
+    let val = [];
+    for (let a = 0; a < letters.length; a++) {
+      val.push(letters[a][j]);
     }
-    //if case above is false, try flipping
-    const verticalToHorizontal = [];
-    for (let j = 0; j < letters[0].length; j++) {
-      let val = [];
-      for (let a = 0; a < letters.length; a++) {
-        val.push(letters[a][j]);
-      }
-      verticalToHorizontal.push(val);
-    }
-    let verticalToHorizontalJoin = verticalToHorizontal.map(ls => ls.join(''))
-    for (let l of verticalToHorizontalJoin) {
-        //if current array includes word, return true;
-        if (l.includes(word)) return true;
-    }
-    return false;
-}
-module.exports = wordSearch
+    verticalToHorizontal.push(val);
+  }
 
-// const horizontalJoinTwo = verticalToHorizontal.map(ls => ls.join(''))
-// for (let l of horizontalJoinTwo) {
-//     if (l.includes(word)) return true;
-// }
-
-// let reverse = letters.reverse();
-// // console.log(reverse);
+  let verticalToHorizontalJoin = verticalToHorizontal.map(ls => ls.join(''));
+  for (let l of verticalToHorizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+  const reverseEach = letters.map(r => r.reverse());
+  const reverseJoin = reverseEach.map(ls => ls.join(''));
+  for (let l of reverseJoin) {
+    if (l.includes(word)) return true;
+  }
+  const reverseEachTwo = verticalToHorizontal.map(r => r.reverse());
+  const reverseJoinTwo = reverseEachTwo.map(ls => ls.join(''));
+  for (let l of reverseJoinTwo) {
+    if (l.includes(word)) return true;
+  }
+  return false;
+};
+module.exports = wordSearch;
 
 
-// // const reverseJoin = reverse.map(ls => ls.join(''))
-// for (let l of letters.reverse().join('')) {
-//     if (l.includes(word)) return true;
-// }
-
-// // const reverseTwo = verticalToHorizontal.reverse();
-
-// // const reverseJoinTwo = reverseTwo .map(ls => ls.join(''))
-// for (let l of horizontalJoinTwo.reverse().join('')) {
-//     if (l.includes(word)) return true;
-// }
 
